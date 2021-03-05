94dcdf9bd620080d571cb37afb07d541f6f1c086
anggaalfiansah <anggaalfiansah26@gmail.com>
2021-03-05 15:11:38 +0700

    edit cors

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var cors = require('cors');
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
--- routes/users.js
+++ routes/users.js
@@ -1,103 +1,41 @@
 var express = require("express");
+var cors = require('cors');
 var router = express.Router();
 var UserController = require("../Controllers/UserController");
 
 // Get All User
 router
-  .route("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-    );
-  })
+  .route("/", cors())
   .get(UserController.getAllUser);
 
 // Add User
 router
-  .route("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-    );
-  })
+  .route("/", cors())
   .post(UserController.addUser);
 
 // Update Data By ID
 router
-  .route("/:id", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-    );
-  })
+  .route("/:id", cors())
   .patch(UserController.updateUserById);
 
 // Get User by Username
 router
-  .route("/username/:username", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-    );
-  })
+  .route("/username/:username", cors())
   .get(UserController.getUserByUsername);
 
 // Get User by email
 router
-  .route("/email/:email", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-    );
-  })
+  .route("/email/:email", cors())
   .get(UserController.getUserByEmail);
 
 // Get User by Phone
 router
-  .route("/phone/:phone", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-    );
-  })
+  .route("/phone/:phone", cors())
   .get(UserController.getUserByPhone);
 
 // Get User by Address
 router
-  .route("/address/:address", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-    );
-  })
+  .route("/address/:address", cors())
   .get(UserController.getUserByAddress);
 
 module.exports = router;
