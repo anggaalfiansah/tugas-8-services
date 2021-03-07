2df5929209879f5338484d55d7d81440daf6c8cc
anggaalfiansah <anggaalfiansah26@gmail.com>
2021-03-07 14:31:36 +0700

    Fix Cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
+
 
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
@@ -36,6 +38,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
